ae2b52b42626be2b91e61969522cf14dd39ba79e
lxue <30921318+titxue@users.noreply.github.com>
2021-08-25 13:04:44 +0800

    Update img250.py

--- img250.py
+++ img250.py
@@ -7,6 +7,7 @@ class PosterSpider(object):
     def __init__(self, path='./'):
         # 保存路径
         self.path = path
+        # 设置 User-Agent
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
         }
